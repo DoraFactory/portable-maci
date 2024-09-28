c753533be23e21f0ed73378cda656952bbe161fe
99Kies <1290017556@qq.com>
2024-09-28 14:18:18 +0800

    update rpc endpoint

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -108,7 +108,7 @@ let configInstance = {
 
   api:
     config.network === 'VOTA_TEST'
-      ? 'https://3.0.94.169:3000/'
+      ? 'http://3.0.94.169:3000/'
       : 'https://vota-api.dorafactory.org/',
   chainInfo: config.network === 'VOTA_TEST' ? votaTestChainInfo : votaChainInfo,
   detailUrl:
