11f1b60832ab3f1ae2c4dfb37b3882cfcff921f5
99Kies <1290017556@qq.com>
2025-02-08 15:55:08 +0800

    update oracle maci code_id

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -149,7 +149,7 @@ export function getConfig() {
         ? 'https://vota-testnet-api.dorafactory.org/'
         : 'https://vota-api.dorafactory.org/',
     chainInfo: network === 'VOTA_TEST' ? votaTestChainInfo : votaChainInfo,
-    oracleCodeId: network === 'VOTA_TEST' ? ['102', '105', '108', '110'] : ['101'],
+    oracleCodeId: network === 'VOTA_TEST' ? ['102', '105', '108', '110', '113'] : ['101'],
     oracleApi:
       network === 'VOTA_TEST'
         ? 'https://vota-testnet-certificate-api.dorafactory.org'
